c9190d1ca12f7c6a92d4c6830315c35ef27d4e94
MD Ariful Islam <115591569+arif9011@users.noreply.github.com>
2024-01-24 17:45:12 +0000

    Delete question_1.sql

--- question_1.sql
+++
@@ -1,9 +0,0 @@
--- Q1. How many staff are there in all of the UK stores?
-SELECT * FROM dim_store;
-
-SELECT 
-    COUNT(staff_numbers) AS total_uk_staff
-FROM dim_store
-WHERE country = 'UK';
-
--- Total number of UK staff is 265.
